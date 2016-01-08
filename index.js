b8c807f3f62834ca47828e2971f8a0d88d70fc0b
christian-bromann <contact@christian-bromann.com>
2016-01-08 03:54:59 +0100

    reimplemented deployment functionality

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ hexo.extend.console.register(COMMAND_NAME, pkg.description, {
     alias: COMMAND_ALIAS,
     usage: COMMAND_USAGE,
     arguments: [
-        { name: 'build', desc: 'npm run clean && npm install && npm run copy-docs && npm run generate-markdown && npm run generate && npm run compass && npm run compress-css && npm run compress-js && npm run remove-obsoletes && npm run webmastertools' },
-        { name: 'webmastertools', desc: ' echo \"google-site-verification: googleb498eedc81b2abab.html\" > ./public/googleb498eedc81b2abab.html' },
-        { name: 'clean', desc: 'rm -fr ./public ./source/api ./node_modules' },
-        { name: 'generate', desc: 'hexo generate' },
-        { name: 'remove-obsoletes', desc: 'rm -fr ./public/css/config.rb ./public/css/screen.scss' },
-        { name: 'compress-css', desc: 'yuicompressor ./public/css/screen.css > ./public/css/tmp.css && mv ./public/css/tmp.css ./public/css/screen.css' },
-        { name: 'compress-js', desc: 'yuicompressor ./public/js/app.js -o ./public/js/tmp.js && mv ./public/js/tmp.js ./public/js/app.js' },
-        { name: 'compass', desc: 'rm -f themes/webdriver.io/source/css/screen.css && cd themes/webdriver.io/source/css && compass compile && cd ../../../../' },
-        { name: 'copy-docs', desc: 'cp -r node_modules/webdriverio/docs/** source/' },
-        { name: 'generate-markdown', desc: 'mkdir ./source/api && npm run generate-protocol-commands && npm run generate-action-commands' },
-        { name: 'generate-protocol-commands', desc: './node_modules/.bin/wddoc -i ./node_modules/webdriverio/lib/protocol/**/*.js -o ./source/api -t ./node_modules/wddoc/templates/template.md.ejs' },
-        { name: 'generate-action-commands', desc: './node_modules/.bin/wddoc -i ./node_modules/webdriverio/lib/commands/**/*.js -o ./source/api -t ./node_modules/wddoc/templates/template.md.ejs' }
+        { name: 'build', desc: 'kicks of complete build process' },
+        { name: 'clean', desc: 'clean public folder, markdown files and node_modules directory' },
+        { name: 'install', desc: 'installs page dependencies' },
+        { name: 'getDocs', desc: 'copies over markdown files from webdriverio package' },
+        { name: 'generateActionCommands', desc: 'generate action commands with wddoc' },
+        { name: 'generateProtocolCommands', desc: 'generate protocol commands with wddoc' },
+        { name: 'generateMarkdown', desc: 'generates markdown files from webdriverio package' },
+        { name: 'compass', desc: 'compiles sass files' },
+        { name: 'generate', desc: 'generate page with hexo' },
+        { name: 'compressCSS', desc: 'minifies CSS' },
+        { name: 'compressJS', desc: 'minifies JS' },
+        { name: 'webmastertools', desc: 'generates webmastertools verification' }
     ]
 }, async (command) => {
     let build = new Build(command, hexo)
